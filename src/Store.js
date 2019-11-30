19ab699ec59021ab1dc89d6c31e58b73245ba6ef
MediEl <mehdielallam07@gmail.com>
2019-11-30 21:37:17 +0100

    Adding Redux store file

---
+++ src/Store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./redux";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const initialState = {};
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
+
+export default store;
