a487408d7937660499755db11bc69736f8cf53ec
MediEl <mehdielallam07@gmail.com>
2019-12-01 16:08:36 +0100

    Adding localStorage to the initial state

--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./redux";
+const initialState = {};
+if (localStorage.getItem("cartItems")) {
+  initialState.cart = {
+    cartItems: JSON.parse(localStorage.getItem("cartItems"))
+  };
+}
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const initialState = {};
+
 const store = createStore(
   rootReducer,
   initialState,
